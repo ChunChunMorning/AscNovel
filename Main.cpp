59cefce6b777d4face11135c1711d296c014556e
Asachun <chuchuchu.chunchun@gmail.com>
2016-05-20 00:57:28 +0900

    add test program

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,12 @@
 
 void Main()
 {
+	Window::Resize(1280, 720);
+
+	TextureAsset::Register(L"test_text_box", L"test_text_box.png");
+	FontAsset::Register(L"test_name", 30, L"メイリオ");
+	FontAsset::Register(L"test_text", 30, L"メイリオ");
+
 	asc::Novel novel;
 
 	while (System::Update())
--- asc/AscNovel.hpp
+++ asc/AscNovel.hpp
@@ -3,10 +3,21 @@
 
 namespace asc
 {
+	using namespace s3d;
+
+	using FontAssetName = String;
+
 	class Novel
 	{
 	public:
-		Novel() = default;
+		Novel() :
+			m_nameFont(L"test_name"),
+			m_textFont(L"test_text"),
+			m_textBoxRegion(6, 440, 1268, 285),
+			m_namePosition(40, 525),
+			m_textPosition(60, 575),
+			m_name(L"名前"),
+			m_text(L"テキスト") {}
 
 		virtual ~Novel() = default;
 
@@ -14,9 +25,29 @@ namespace asc
 
 		void update() {}
 
-		void draw() const {}
+		void draw() const
+		{
+			m_textBoxRegion(TextureAsset(L"test_text_box")).draw();
+
+			FontAsset(m_nameFont).draw(m_name, m_namePosition, Palette::Black);
+			FontAsset(m_textFont).draw(m_text, m_textPosition, Palette::Black);
+		}
 
 	private:
+
+		FontAssetName m_nameFont;
+
+		FontAssetName m_textFont;
+
+		Rect m_textBoxRegion;
+
+		Point m_namePosition;
+
+		Point m_textPosition;
+
+		String m_name;
+
+		String m_text;
 
 	};
 }
