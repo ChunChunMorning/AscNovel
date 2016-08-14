02df27526726aced0579cfd5cda74e3b21ec83cc
Asachun <chuchuchu.chunchun@gmail.com>
2016-08-14 21:13:33 +0900

    Implement add and delete Sprite function.

--- asc/AscSpriteManager.hpp
+++ asc/AscSpriteManager.hpp
@@ -20,9 +20,30 @@ namespace asc
 			m_sprites.push_back(std::make_unique<Sprite>(2, L"character2", RectF(640, 0, 640, 720)));
 			m_sprites.push_back(std::make_unique<FixedSprite>(3, L"character3", RectF(480, 180, 320, 360)));
 			m_sprites[0]->turnOn();
+			deleteSprite(3);
 		}
 
 		virtual ~SpriteManager() = default;
+
+		void addSprite(int id, const String& texture, const RectF& region)
+		{
+			m_sprites.push_back(std::make_unique<Sprite>(id, texture, region));
+		}
+
+		void addFixedSprite(int id, const String& texture, const RectF& region)
+		{
+			m_sprites.push_back(std::make_unique<FixedSprite>(id, texture, region));
+		}
+
+		void deleteSprite(int id)
+		{
+			Erase_if(m_sprites, [&](const std::unique_ptr<Sprite>& sprite){ return id == sprite->getID(); });
+		}
+
+		void clearSprite()
+		{
+			m_sprites.clear();
+		}
 
 		void draw() const
 		{
